802ef26ee889526922a00a98aec4ce15265b5048
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-02-14 14:46:09 +0300

    NUnit test project added.

---
+++ SecurityEncryption.Tests/CipherAlgorithmaTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace SecurityEncryption.Tests
+{
+    public class CipherAlgorithmaTest
+    {
+
+        [SetUp]
+        public void Setup()
+        { }
+
+        [Test]
+        public void DesCipher_Encryption_Test()
+        {
+            var encryption = DesCipher.Encryption("VeyselMUTLU");
+            var descryption = DesCipher.Descryption(encryption);
+
+            Assert.AreEqual("VeyselMUTLU", descryption);
+        }
+
+    }
+}
