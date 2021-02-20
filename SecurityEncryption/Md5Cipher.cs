2bfbc18b3404cc56726f262d7655300fd367e4bd
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-02-20 15:22:28 +0300

    MD5 cipher added

---
+++ SecurityEncryption/Md5Cipher.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace SecurityEncryption
+{
+    public static class Md5Cipher
+    {
+        /*
+         * By Veysel MUTLU -> github.com/vmutlu
+         * veysel_mutlu42@hotmail.com
+         * 
+         * Sample Usage
+         * var encrypt = Md5Cipher.Encryption("Şifrelecek Metin");
+         */
+
+        /// <summary>
+        /// The method returned by encrypting the text received as parameter with the MD5 encryption algorithm
+        /// </summary>
+        /// <param name="encrypting">Şifrelenecek Metin...</param>
+        /// <returns></returns>
+        public static string Encryption(string encrypting)
+        {
+            MD5CryptoServiceProvider mD5CryptoServiceProvider = new MD5CryptoServiceProvider();
+            byte[] encryptionArray = Encoding.UTF8.GetBytes(encrypting);
+            encryptionArray = mD5CryptoServiceProvider.ComputeHash(encryptionArray);
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (byte encry in encryptionArray)
+                stringBuilder.Append(encry.ToString("x2").ToLower());
+
+            return stringBuilder.ToString();
+        }
+    }
+}
